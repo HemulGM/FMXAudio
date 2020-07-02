199dee7e04a35a86f93c5967ecc5b24dbb10c749
alinvip22@gmail.com <HemulGM>
2020-07-02 08:47:15 +0700

    Clear

--- Sources/FMX.Player.Windows.pas
+++ Sources/FMX.Player.Windows.pas
@@ -2,47 +2,28 @@ unit FMX.Player.Windows;
 
 interface
 
+{$IFDEF MSWINDOWS}
 uses
   FMX.Player.Shared, FMX.Platform.Win, FMX.BASS, FMX.BASS.AAC;
 
 type
   TFMXPlatformPlayer = class(TFMXCustomPlayer)
   protected
-    function FInitBass(Handle: Cardinal): Boolean;
     function InitBass(Handle: Pointer): Boolean; override;
-  public
-    function Init(Handle: Cardinal): Boolean; overload;
   end;
+{$ENDIF}
 
 implementation
 
 { TFMXPlatformPlayer }
 
-function TFMXPlatformPlayer.FInitBass(Handle: Cardinal): Boolean;
-begin
-  Result := BASS_Init(Device, Freq, Flags, Handle, nil);
-end;
-
-function TFMXPlatformPlayer.Init(Handle: Cardinal): Boolean;
-begin
-  Result := False;
-  if BASS_Available then
-  begin
-    if FInitBass(Handle) then
-    begin
-      BASS_PluginLoad(PChar(BASS_AAC_Lib), 0 or BASS_UNICODE);
-      BASS_SetConfig(BASS_CONFIG_NET_PLAYLIST, 1);
-      BASS_SetConfig(BASS_CONFIG_NET_PREBUF, 0);
-      Result := True;
-    end;
-  end;
-  FIsInit := Result;
-end;
+{$IFDEF MSWINDOWS}
 
 function TFMXPlatformPlayer.InitBass(Handle: Pointer): Boolean;
 begin
   Result := BASS_Init(Device, Freq, Flags, WindowHandleToPlatform(Handle).Wnd, nil);
 end;
 
+{$ENDIF}
 end.
 
